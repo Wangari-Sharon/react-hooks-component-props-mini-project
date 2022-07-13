990377c43810767314e321926069443fea16b421
Wangari-Sharon <wangarik35@gmail.com>
2022-07-13 17:58:39 +0300

    commit tree 52115c245d04ab587e1d9481061f0ea06d320a92

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "../components/Header";
+import About from "../components/About";
+import logo from "./Images/Wangari.jpeg"
+import ArticleList from "./ArticleList";
 
-console.log(blogData);
+console.log(blogData.posts);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={logo} about={"Personal blog by"} 
+       name="Wangari Sharon"
+       texttwo="React is amazing!"
+       />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
--- src/data/blog.js
+++ src/data/blog.js
@@ -1,9 +1,9 @@
 import logo from "../assets/logo.svg";
 
 const blogData = {
-  name: "Underreacted",
+  name: "Underrated",
   image: logo,
-  about: "A blog about learning React",
+  about: "Documenting my journey about learning React",
   posts: [
     {
       id: 1,
